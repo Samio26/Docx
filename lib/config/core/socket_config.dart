e21f8a339f2d81b86936290dd4d8075496cc9960
sami <samio@MacBook-Air-sami.local>
2024-05-13 15:09:37 +0100

    adding the front part

---
+++ lib/config/core/socket_config.dart
@@ -0,0 +1,16 @@
+
+import 'package:socket_io_client/socket_io_client.dart';
+import 'package:testt/config/clients/socket_client.dart';
+
+class SocketConfig{
+final  _socketclient=SocketClient.instance.socket!;
+
+Socket get socket=>_socketclient;
+void joinRoom(String docId){
+  _socketclient.emit("join",docId);
+}
+void getContentTyping( Map<dynamic,String> data){
+_socketclient.emit("writing",data);
+  
+}
+}
